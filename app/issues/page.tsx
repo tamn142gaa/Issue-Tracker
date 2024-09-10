d5f0c504eed2473b58f33309b298c4c903b016fb
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-10 23:39:48 +0100

    Building the new issue page.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return (
+    <div>
+      <Button>
+        <Link href="issues/new">New Isse</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default IssuePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { Theme } from "@radix-ui/themes";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,8 +31,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main className="p-5">{children}</main>
+        </Theme>
       </body>
     </html>
   );
