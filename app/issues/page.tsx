406c5e8a63a44029ad4aedda22feac762e330ced
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-16 14:01:39 +0100

    Building the showing of the issues.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,34 @@
-import { Button } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const IssuePage = () => {
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
     <div>
       <Button>
         <Link href="issues/new">New Issue</Link>
       </Button>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>{issue.status}</Table.Cell>
+              <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+          <Table.Cell></Table.Cell>
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
