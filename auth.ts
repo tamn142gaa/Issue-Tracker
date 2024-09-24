ed1cf2a81c5027eade397454f04719b59a22c2e5
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-24 04:05:15 +0100

    Configuring Google Provider & Adding the Prisma Adapter.

--- auth.ts
+++ auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
+import prisma from "./prisma/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
 });
