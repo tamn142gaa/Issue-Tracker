eb14ff43b86dafd81e60228050d41a6b71b13d68
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-27 23:05:37 +0100

    Building the filter component.

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import React from "react";
 import { Table } from "@radix-ui/themes";
-import IssueActions from "./IssueActions";
+import IssueActions from "./issues/IssueActions";
 
 const LoadingIssuePage = () => {
   const issues = [1, 2, 3, 4, 5, 6];
