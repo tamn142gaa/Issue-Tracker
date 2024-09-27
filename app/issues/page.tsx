155068eb05dbdcee4ea96ffb84b9de0185829f76
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-27 23:20:50 +0100

    Filtering issues.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,23 @@
 import prisma from "@/prisma/client";
+import { Issue, Status } from "@prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "../components";
 import IssueActions from "./IssueActions";
 
-const IssuePage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status; orderBy: keyof Issue };
+}
+
+const IssuePage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
+
   return (
     <div>
       <IssueActions />
