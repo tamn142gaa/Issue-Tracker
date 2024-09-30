1767b4878cfabb590977fd733ea7c33268ea4c2c
Youssouf Cherif Bichara <youssoufcherifbich@gmail.com>
2024-09-30 23:25:53 +0100

    Building the layout of the pagination and implementing pagination.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+import Pagination from "./components/Pagination";
+
+interface Props {
+  searchParams: { page: string };
+}
+
+export default function Home({ searchParams }: Props) {
+  return (
+    <Pagination
+      itemCount={2}
+      currentPage={parseInt(searchParams.page)}
+      pageSize={2}
+    />
+  );
 }
